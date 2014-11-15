b9fb4885492103008b70eddebd69e853b88a5ed1
michael <michael.gissler@gmail.com>
2014-11-15 22:41:24 +0100

    added a basic nancy selfhosting sample

---
+++ HelloNancy/HelloNancy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Nancy.Hosting.Self;
+using Nancy;
+
+namespace HelloNancy
+{
+    public class Program    
+    {
+        public static void Main(string[] args)
+        {
+            // initialize an instance of NancyHost (found in the Nancy.Hosting.Self package)
+            var host = new NancyHost(new Uri("http://localhost:12345"));
+            host.Start(); // start hosting
+
+            Console.ReadKey();
+            host.Stop();  // stop hosting
+        }
+    }
+
+
+
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = x => "Hello World";
+        }
+    }
+}

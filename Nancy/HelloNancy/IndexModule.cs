d856d77897b0133e2d1d84079e529ed2f3171c01
michael <michael.gissler@gmail.com>
2014-11-20 22:47:02 +0100

    added new solution with two simple nancy projects

---
+++ Nancy/HelloNancy/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿namespace HelloNancy
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
---
+++ Nancy/NineStars/IndexModule.cs
@@ -0,0 +1,20 @@
+﻿namespace NineStars
+{
+    using Nancy;
+
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+
+            Get["/index.html"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}

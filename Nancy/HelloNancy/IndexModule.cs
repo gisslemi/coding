d937f6a25e55421c7b29d19e4986f7c7af831f17
michael <michael.gissler@gmail.com>
2014-11-22 20:11:57 +0100

    basic nancy application as a starting point

--- Nancy/HelloNancy/IndexModule.cs
+++ Nancy/HelloNancy/IndexModule.cs
@@ -1,6 +1,8 @@
 ﻿namespace HelloNancy
 {
+    using HelloNancy.Models;
     using Nancy;
+    using System.Collections.Generic;
 
     public class IndexModule : NancyModule
     {
@@ -9,6 +11,16 @@
             Get["/"] = parameters =>
             {
                 return View["index"];
+            };
+
+            Get["/cars"] = parameters =>
+            {
+                List<Car> cars = new List<Car>();
+                cars.Add(new Car { HorsePower=123, Make="BMW", YearOfBuild=2010 });
+                cars.Add(new Car { HorsePower = 123, Make = "BMW", YearOfBuild = 2010 });
+                cars.Add(new Car { HorsePower = 123, Make = "BMW", YearOfBuild = 2010 });
+                cars.Add(new Car { HorsePower = 123, Make = "BMW", YearOfBuild = 2010 });
+                return View["cars",cars];
             };
         }
     }

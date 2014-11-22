0ed80994d9767e3c1bbf656a51b1a6055f9f5fce
michael <michael.gissler@gmail.com>
2014-11-22 20:12:54 +0100

    added cars model and cars view

---
+++ Nancy/HelloNancy/Models/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloNancy.Models
+{
+    public class Car
+    {
+        public int HorsePower { get; set; }
+        public string Make { get; set; }
+        public int YearOfBuild { get; set; }
+    }
+}
